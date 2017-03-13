e08b5b7f5d89a82304a91901e1b799def8ccf2a5
TheVaffel <hkon20@hotmail.com>
2017-03-13 18:46:45 +0100

    Gave the main file a more sensible name

--- src/project/Treningslogger.java
+++ src/project/Treningslogger.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;  
 
-class LoadDriver{  
+class Treningslogger{  
 	
 	static Scanner scanner;
 	static Connection con;
